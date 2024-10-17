7f6523749fb0c0109ec2255e2549e36ecc3200a3
Luiz G R Freitas <luizfreitasaca@gmail.com>
2024-10-16 21:36:57 -0300

    Removing pytest from requirements

--- src/tests/example_test/test_successful.py
+++
@@ -1,7 +0,0 @@
-from tests.SeleniumCommon import SeleniumCommon
-
-class TestSuccessful(SeleniumCommon):
-
-    def test_google(self):
-        self.driver.get("https://www.google.com")
-        assert "Google" in self.driver.title
